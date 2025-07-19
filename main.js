9e98900717fd83b141741ad98ee53368cbd332ad
Mohamed Abdirahman <mohamedepdirahman@gmail.com>
2025-07-19 17:50:14 +0300

    created files

---
+++ main.js
@@ -0,0 +1,28 @@
+const btnmem = document.querySelector("button#btn")
+const pop = document.querySelector("div.pop")
+const call = document.querySelector("button.btnh")
+
+
+
+console.log(call)
+
+btnmem.addEventListener("click", () => {
+    pop.classList.toggle("active")
+    if(pop.className === "pop active"){
+       setTimeout(() => {
+    pop.classList.remove("active")
+}, 4000);
+    }
+})
+
+call.addEventListener("click", ()=>{
+
+        pop.classList.toggle("active")
+    if(pop.className === "pop active"){
+       setTimeout(() => {
+    pop.classList.remove("active")
+}, 4000);
+    }
+})
+ 
+
